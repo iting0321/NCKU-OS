58a3c4f5bf451abdbd1e8d2cf2c1fa7c8bde72f1
iting0321 <0321iting@gmail.com>
2024-12-02 22:10:48 +0800

    complete : 3_2

--- 3/3_2/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/iting/Desktop/NCKU-OS-1/3/3_2/modules.order := {   echo /home/iting/Desktop/NCKU-OS-1/3/3_2/My_Kernel.o; :; } > /home/iting/Desktop/NCKU-OS-1/3/3_2/modules.order
--- 3/3_2/modules.order
+++
@@ -1,1 +0,0 @@
-/home/iting/Desktop/NCKU-OS-1/3/3_2/My_Kernel.o
